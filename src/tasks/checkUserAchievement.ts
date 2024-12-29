4e7aa1201f0a555ba44dd1951d16aae3f93fc367
Ana Beatriz <184788721+AnaBeatrizJBSan@users.noreply.github.com>
2024-12-29 02:06:59 -0300

    feat: add new files

---
+++ src/tasks/checkUserAchievement.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import { getDetailedUserProfile } from "../services/users";
+import { userIds } from "./userIds";
+
+const checkUserAchievement = async (userID: string) => {
+  const ACHIEVEMENT_CODE = "ACH_TradingPass2";
+  const userDetailedProfile = await getDetailedUserProfile(userID);
+
+  if (userDetailedProfile) {
+    const userHasTradingPassAchievement = userDetailedProfile.badges.find(
+      ({ code }) => code === ACHIEVEMENT_CODE,
+    );
+
+    if (!userHasTradingPassAchievement) {
+      const fileContent = `'${userDetailedProfile.user.name}',\n`;
+
+      fs.appendFile(
+        "src/tasks/usersWithoutAchievement.ts",
+        fileContent,
+        (error) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(
+              `user ${userDetailedProfile.user.name} has not this achievement`,
+            );
+          }
+        },
+      );
+    }
+  }
+};
+
+userIds.forEach((userId, index) => {
+  setTimeout(async () => {
+    await checkUserAchievement(userId);
+  }, index * 10000);
+});
